a2bd5dfcbaee180616c24bdee01dc9c9b044edd2
Mohamed Mohamud <m.mohamud1@hotmial.com>
2021-05-12 11:58:41 +0100

    add nav and footer

--- footer.php
+++ footer.php
@@ -11,21 +11,68 @@
 
 ?>
 
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'qo' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'qo' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'qo' ), 'qo', '<a href="http://underscores.me/">Underscores.me</a>' );
-				?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
+<footer class="foot-container">
+      <div class="foot-wrap">
+        <!--Footer Links-->
+        <div class="foot-links-container">
+          <div class="foot-links-wrapper">
+            <div class="foot-links-item">
+              <h1 class="foot-links-title">Quod Orbis office:</h1>
+              <p class="foot-link-p">4th Floor</p>
+              <p class="foot-link-p">Silverstream House,</p>
+              <p class="foot-link-p">45 Fitzroy Street,</p>
+              <p class="foot-link-p">Fitzrovia,</p>
+              <p class="foot-link-p">London W1T 6EB</p>
+            </div>
+            <div class="foot-links-item">
+              <h1 class="foot-links-title">Continuous Controls Monitoring</h1>
+              <a href="https://www.quodorbis.com/ccm#about-ccm" class="foot-link">What is CCM?</a>
+              <a href="https://www.quodorbis.com/ccm.html#ccm-solution" class="foot-link">QO CCM Solution</a>
+              <a href="https://www.quodorbis.com/ccm.html#qo-platform" class="foot-link">QO Platform & Case Study</a>
+              <a href="https://www.quodorbis.com/ccm.html#ccm-readiness" class="foot-link">QO CCM Readiness Solution</a>
+              <a href="https://www.quodorbis.com/ccm.html#digital-risk" class="foot-link">Digital Risk: Board-level Issue</a>
+              <a href="https://www.quodorbis.com/ccm.html#business-cost" class="foot-link">CCM Business Case & Cost</a>
+            </div>
+            <div class="foot-links-item">
+              <h1 class="foot-links-title">Cyber Security Services</h1>
+              <a href="https://www.quodorbis.com/digital-cyber-consultancy.html#cyber" class="foot-link">Cyber Security Consultancy</a>
+              <a href="https://www.quodorbis.com/digital-cyber-consultancy.html#transformation" class="foot-link">Transformation Consultancy</a>
+              <a href="https://www.quodorbis.com/digital-cyber-consultancy.html#innovation" class="foot-link">Innovation Consultancy</a>
+            </div>
+            <div class="foot-links-item">
+              <h1 class="foot-links-title">About Us</h1>
+              <a href="https://www.quodorbis.com/about.html#aboutqo" class="foot-link">About Quod Orbis</a>
+              <a href="https://www.quodorbis.com/about.html#team_ex" class="foot-link">Our People</a>
+              <a href="https://www.quodorbis.com/about.html#careers" class="foot-link">Careers at Quod Orbis</a>
+            </div>
+            <div class="foot-links-item">
+              <a data-toggle="modal" data-target="#demoModal" class="foot-links-title foot-links-title-link">Request A Demo</a>
+              <a href="https://www.quodorbis.com/contact.html" class="foot-links-title foot-links-title-link">Contact</a>
+              <a href="https://www.quodorbis.com/news.html" class="foot-links-title foot-links-title-link">News & Views</a>
+            </div>
+          </div>
+        </div>
+        <!--Social Media & Copyright-->
+        <section class="foot-social">
+          <div class="foot-social-wrap">
+            <div class="foot-logo">
+              <a href="https://www.quodorbis.com/">
+                <img src="https://www.quodorbis.com/img/white_logo_transparent.png" alt="logo" class="foot-logo-img" >
+              </a>
+            </div>
+            <small class="foot-copy">Quod Orbis &copy; 2021</small>
+            <div class="foot-social-icons">
+              <a href="mailto:contact@quodorbis.com" class="foot-social-icon-link"><i class="fa fa-envelope"></i></a>
+              <a href="tel:+442039622206" class="foot-social-icon-link"><i class="fa fa-phone"></i></a>
+              <a href="https://www.linkedin.com/company/quod-orbis/" class="foot-social-icon-link" target="_blank"><i class="fab fa-linkedin-in"></i></a>
+              <a href="https://twitter.com/QuodOrbis" class="foot-social-icon-link" target="_blank"><i class="fab fa-twitter"></i></a>
+            </div>
+
+          </div>
+        </section>
+
+      </div>
+    </footer>
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
--- header.php
+++ header.php
@@ -16,6 +16,9 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.15.3/css/fontawesome.min.css" integrity="sha384-wESLQ85D6gbsF459vf1CiZ2+rr+CsxRY0RpiF1tLlQpDnAgg6rwdsUF1+Ics2bni" crossorigin="anonymous">
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js" integrity="sha384-+YQ4JLhjyBLPDQt//I+STsc9iw4uQqACwlvpslubQzn4u2UU2UFM80nGisd026JF" crossorigin="anonymous"></script>
 
 	<?php wp_head(); ?>
 </head>
@@ -45,15 +48,152 @@
 			<?php endif; ?>
 		</div><!-- .site-branding -->
 
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'qo' ); ?></button>
-			<?php
-			wp_nav_menu(
-				array(
-					'theme_location' => 'menu-1',
-					'menu_id'        => 'primary-menu',
-				)
-			);
-			?>
-		</nav><!-- #site-navigation -->
+		<nav class="nav nav-2">
+      <div class="nav-container">
+        <a href="https://www.quodorbis.com/" class="nav-logo nav-2-logo" id="nav-logo">
+          <img src="https://www.quodorbis.com/img/white_logo_transparent.png" alt="logo" class="nav-logo-img" >
+        </a>
+        <button class="nav-mobile" id="hamburger-icon">
+          <i class="fas fa-bars"></i>
+       </button>
+
+       <ul class="nav-menu nav-2-menu" >
+         <li class="nav-items">
+           <a href="https://www.quodorbis.com/" class="nav-links">Home</a>
+         </li>
+         <li class="nav-items dropdown-items">
+          <a href="https://www.quodorbis.com/about.html" class="nav-links dropdown-link">About Us &nbsp;<i class="fas fa-caret-down"></i></a>
+          <div class="dropdown-content dropdown-content-2">
+            <a href="https://www.quodorbis.com/about.html#aboutqo">About Quod Orbis</a>
+            <a href="https://www.quodorbis.com/about.html#team_ex">Our People</a>
+            <a href="https://www.quodorbis.com/about.html#careers">Careers</a>
+          </div>
+        </li>
+         <li class="nav-items dropdown-items">
+          <a href="https://www.quodorbis.com/ccm.html" class="nav-links dropdown-link">Continuous Controls Monitoring &nbsp;<i class="fas fa-caret-down"></i></a>
+          <div class="dropdown-content">
+            <a href="https://www.quodorbis.com/ccm.html#about-ccm">What is CCM?</a>
+            <a href="https://www.quodorbis.com/ccm.html#ccm-solution">QO CCM Solution</a>
+            <a href="https://www.quodorbis.com/ccm.html#qo-platform">QO Platform & Case Study</a>
+            <a href="https://www.quodorbis.com/ccm.html#ccm-readiness">QO CCM Readiness Solution</a>
+            <a href="https://www.quodorbis.com/ccm.html#digital-risk">Digital Risk: Board-level Issue</a>
+            <a href="https://www.quodorbis.com/ccm.html#business-cost">CCM Business Case & Cost</a>
+          </div>
+        </li>
+        <li class="nav-items">
+          <a href="https://www.quodorbis.com/digital-cyber-consultancy.html" class="nav-links">Digital & Cyber Consultancy</a>
+        </li>
+        
+        <li class="nav-items">
+          <a href="https://www.quodorbis.com/news.html" class="nav-links">News & Views</a>
+        </li>
+      </ul>
+
+       <div class="nav-btns nav-2-btns" >
+        <a href="https://www.quodorbis.com/contact.html" class="contact-btn"><button class="nav-btn">Contact Us</button></a>
+         <button class="nav-btn" data-toggle="modal" data-target="#demoModal">Request Demo</button>
+       </div>
+       
+      </div>
+
+    </nav>
+    <!-- /.navbar -->
+    <!--Hamburger-->
+    <aside class="ham-container" id="ham-container">
+      <div class="ham-close-icon" id="ham-close">
+        <i class="fas fa-times"></i>
+      </div>
+      <div class="ham-wrapper">
+        <ul class="ham-menu">
+          <a href="https://www.quodorbis.com//" class="ham-link" id="home-link">Home</a>
+          <div class="ham-dropdown">
+            <a  id="about-link" class="ham-link">About Us &nbsp;<i class="fas fa-caret-down"></i></a>
+            <div class="ham-dropdown-content" id="about-dropdown">
+              <a href="https://www.quodorbis.com/about.html#aboutqo" class="ham-dropdown-link" id="about-link-1">About Us</a>
+              <a href="https://www.quodorbis.com/about.html#team_ex" class="ham-dropdown-link" id="about-link-2">Our People</a>
+              <a href="https://www.quodorbis.com/about.html#careers" class="ham-dropdown-link" id="about-link-3">Careers</a>
+            </div>
+          </div>
+          <div class="ham-dropdown">
+            <a  class="ham-link" id="ccm-link">Continuous Controls Monitoring &nbsp;<i class="fas fa-caret-down"></i></a>
+            <div class="ham-dropdown-content" id="ccm-dropdown">
+              <a class="ham-dropdown-link" id="ccm-link-1" href="https://www.quodorbis.com/ccm.html#about-ccm">What is CCM?</a>
+              <a class="ham-dropdown-link" id="ccm-link-2" href="https://www.quodorbis.com/ccm.html#ccm-solution">QO CCM Solution</a>
+              <a class="ham-dropdown-link" id="ccm-link-3" href="https://www.quodorbis.com/ccm.html#qo-platform">QO Platform & Case Study</a>
+              <a class="ham-dropdown-link" id="ccm-link-4" href="https://www.quodorbis.com/ccm.html#ccm-readiness">QO CCM Readiness Solution</a>
+              <a class="ham-dropdown-link" id="ccm-link-5" href="https://www.quodorbis.com/ccm.html#digital-risk">Digital Risk: Board-level Issue</a>
+              <a class="ham-dropdown-link" id="ccm-link-6" href="https://www.quodorbis.com/ccm.html#business-cost">CCM Business Case & Cost</a>
+            </div>
+
+          </div>
+          <a href="https://www.quodorbis.com/digital-cyber-consultancy.html" class="ham-link" id="dig-link">Digital & Cyber Consultancy</a>
+          <a href="https://www.quodorbis.com/news.html" class="ham-link" id="news-link">News & Views</a>
+        </ul>
+        <div class="ham-btns">
+          <a href="https://www.quodorbis.com/contact.html" class="nav-btn ham-btn" id="contact-link">Contact Us</a>
+         <button class="nav-btn ham-btn" data-toggle="modal" data-target="#demoModal" id="demo-link">Request Demo</button>
+        </div>
+
+      </div>
+    </aside>
+	<!--Modals-->
+	<div class="modal fade" id="eventModal" role="dialog">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h4 class="modal-title">Register for our <b>event</b></h4>
+          </div>
+          <div class="modal-body">
+            <br/>
+            <script charset="utf-8" type="text/javascript" src="//js.hsforms.net/forms/v2.js"></script>
+            <script>
+              hbspt.forms.create({
+                portalId: "5823873",
+                formId: "512a200e-c83c-452c-9f3f-7b2419ec3661"
+            });
+            </script>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="modal fade" id="demoModal" role="dialog">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h4 class="modal-title">Request a <b>demo</b></h4>
+          </div>
+          <div class="modal-body">
+            <br/>
+            <script charset="utf-8" type="text/javascript" src="//js.hsforms.net/forms/shell.js"></script>
+            <script>
+              hbspt.forms.create({
+                          portalId: "5823873",
+                          formId: "5e1f222f-38b0-4a00-9d0d-b7dafa0cbfee"
+            });
+            </script>
+          </div>
+        </div>
+      </div>
+    </div>
+
+	<div class="modal fade" id="myModal" role="dialog">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h4 class="modal-title">Register with <b>us!</b></h4>
+          </div>
+          <div class="modal-body">
+            <p>Please register your contact details with us to access downloadable content.</p>
+            <script charset="utf-8" type="text/javascript" src="//js.hsforms.net/forms/shell.js"></script>
+            <script>
+              hbspt.forms.create({
+                portalId: "5823873",
+                formId: "3503af54-29ea-4a63-9804-8cda893b670f"
+            });
+            </script>
+          </div>
+        </div>
+      </div>
+    </div>
 	</header><!-- #masthead -->
